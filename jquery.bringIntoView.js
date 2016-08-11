699d5a99ec2b75b893c7c3db56c03edf20a0faf3
Tony Coop <tony@thewholeworldwindow.co.uk>
2016-08-11 09:35:48 +0100

    The scroll direction, up or down, is now taken into account, so that the object is brought just enough into view to make it visible but no more.

--- jquery.bringIntoView.js
+++ jquery.bringIntoView.js
@@ -4,14 +4,26 @@
 
 (function ( $ ) {
 
-	$.fn.bringIntoView = function () {
+	$.fn.bringIntoView = function ( options ) {
+
 		var el = this[ 0 ],
 			top = el.offsetTop,
 			left = el.offsetLeft,
 			width = el.offsetWidth,
 			height = el.offsetHeight,
 			fromTop = top,
-			fromLeft = left;
+			fromLeft = left,
+			scrollPos = null,
+			currentScrollPos = $(window).scrollTop();
+
+		// Default options
+		this.options = {
+			pushTop: 10,
+			pushBottom: 10
+		};
+
+		// Merge options
+		this.options = $.extend( {}, this.options, options );
 
 		// Get the offsets from top and left of the document
 		while ( el.offsetParent ) {
@@ -23,9 +35,15 @@
 		// If the element is already visible. Stop.
 		if ( elementInViewport( el ) ) { return; }
 
+		if ( fromTop > currentScrollPos ) {
+			scrollPos = fromTop - $( window.top ).height() + height + this.options.pushBottom;
+		} else {
+			scrollPos = fromTop - this.options.pushTop;
+		}
+
 		// Otherwise, scroll the page so it's just in view
 		$( 'html, body' ).stop().animate({
-			scrollTop : fromTop - $( window.top ).height() + height + 10
+			scrollTop : scrollPos
 		});
 
 		// Returns true if element is visible in browser window
